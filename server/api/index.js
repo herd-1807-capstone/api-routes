06d8a85c7fa83586547785dc99fbbde03c31a7c1
wildapt01 <pb5191@gmail.com>
2018-10-09 12:09:33 -0400

    modif to route on messages

--- server/api/index.js
+++ server/api/index.js
@@ -1,39 +1,39 @@
-const {admin, db} = require('./firebaseadmin');
+const { admin, db } = require('./firebaseadmin');
 const router = require('express').Router();
 module.exports = router;
 
 // a middleware authenticating a logged-in user and adding that user instance to the request.
-router.use(async (req, res, next) => {
-  try{
-    const {access_token} = req.query;
-    if(!access_token){
-      const error = new Error('Forbidden');
-      error.status = 403;
-      next(error);
-      return;
-    }
-    const decodedToken = await admin.auth().verifyIdToken(access_token);
-    if(!decodedToken){
-      const error = new Error('Forbidden');
-      error.status = 403;
-      next(error);
-      return;
-    }
-    const authUserSnapshot = await db.ref(`/users/${decodedToken.uid}`).once('value');
-    const authUser = authUserSnapshot.val();
-    // a user must be logged-in to retrieve data.
-    if(!authUser){
-      const error = new Error('Auth User Not Found in DB');
-      error.status = 403;
-      next(error);
-      return;
-    }
-    req.authUser = authUser;
-    next();
-  }catch(err){
-    next(err);
-  }
-});
+// router.use(async (req, res, next) => {
+//   try{
+//     const {access_token} = req.query;
+//     if(!access_token){
+//       const error = new Error('Forbidden');
+//       error.status = 403;
+//       next(error);
+//       return;
+//     }
+//     const decodedToken = await admin.auth().verifyIdToken(access_token);
+//     if(!decodedToken){
+//       const error = new Error('Forbidden');
+//       error.status = 403;
+//       next(error);
+//       return;
+//     }
+//     const authUserSnapshot = await db.ref(`/users/${decodedToken.uid}`).once('value');
+//     const authUser = authUserSnapshot.val();
+//     // a user must be logged-in to retrieve data.
+//     if(!authUser){
+//       const error = new Error('Auth User Not Found in DB');
+//       error.status = 403;
+//       next(error);
+//       return;
+//     }
+//     req.authUser = authUser;
+//     next();
+//   }catch(err){
+//     next(err);
+//   }
+// });
 
 router.use('/tours', require('./tours'));
 router.use('/users', require('./users'));
--- server/api/messages.js
+++ server/api/messages.js
@@ -1,22 +1,46 @@
 const { db } = require('./firebaseadmin');
+const firebase = require('firebase');
 const router = require('express').Router();
 module.exports = router;
 
 // POST /api/chat/:userId
 router.post('/:userId', async (req, res, next) => {
   try {
-    const fromId = req.authUser;
-    const { text, tourId } = req.body;
+    // const fromId = req.authUser;
+    const { text, tourId, fromId } = req.body;
     const toId = req.params.userId;
-    const newMessage = { fromId, text, toId };
-    const newKey = await db
-      .ref(`/tours/${tourId}`)
-      .child(`messages`)
-      .push().key;
+    const conversationRef = [fromId + '_' + toId, toId + '_' + fromId];
+    const conversationObj = await db
+      .ref(`/users/${fromId}/conversations`)
+      .orderByValue()
+      .equalTo(conversationRef[0] || conversationRef[1]);
+    const timestamp = firebase.database.ServerValue.TIMESTAMP;
+    const newMessage = {
+      fromId,
+      toId,
+      text,
+      timestamp,
+    };
+    const updates = {};
 
-    const message = {};
-    message[`${newKey}`] = newMessage;
-    await db.ref(`/tours/${tourId}/messages/`).update(message);
+    if (conversationObj.length < 1) {
+      const newMessageKey = db
+        .ref(`/users/${fromId}`)
+        .child('conversation')
+        .push().key;
+      updates[`/users/${fromId}/conversations/${newMessageKey}`] =
+        conversationRef[0];
+      updates[`/users/${toId}/conversations/${newMessageKey}`] =
+        conversationRef[0];
+      updates[`/tours/${tourId}/conversations/${newMessageKey}`] = newMessage;
+    } else {
+      updates[
+        `/tours/${tourId}/conversations/${conversationObj.key}`
+      ] = newMessage;
+    }
+
+    await db.ref().update(updates);
+
     res.status(201);
   } catch (error) {
     next(error);
